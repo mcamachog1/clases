6faaf93e5716865ba0c3f5307f5b88c5cbe3b025
mcamachog1 <63174835+mcamachog1@users.noreply.github.com>
2020-12-30 14:57:03 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Reservas y pagos</title>
+</head>
+<body>
+	<h1>Reservas y pagos</h1>
+	<ul>
+		<li><a href="reservaslist.php">Clases agendadas</a></li>
+		<li><a href="reservasagendar.php">Agendar clase</a></li>
+	</ul>
+</body>
+</html>
